707a70f3e021a01818d8fcd568a13a474f2adba6
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-10-11 13:39:11 +0200

    Add some random variates

--- src/operator.lua
+++ src/operator.lua
@@ -154,4 +154,165 @@ function table.map (tbl, f)
 	return mapped
 end
 
+function math.random_uniform(a, b)
+	a = a or 0
+	b = b or 1
+	return a + (b - a) * math.random()
+end
+
+function math.random_bernoulli (p)
+
+	if math.random() < p then return 1 else return 0 end
+end
+
+function math.random_bernoulli_boolean (p)
+
+	return math.random_bernoulli(p) == 1
+end
+
+function math.random_binomial (n, p)
+
+	local s, B = 0, math.random_bernoulli
+	for i = 1, n do s = s + B (p) end
+	return s
+end
+
+function math.random_geometric (p)
+	
+	local w, B = -1, math.random_bernoulli_boolean
+	repeat w = w + 1 until B (p)
+	return w
+end
+
+--[[
+	def triangular(self, low=0.0, high=1.0, mode=None):
+        """Triangular distribution.
+
+        Continuous distribution bounded by given lower and upper limits,
+        and having a given mode value in-between.
+
+        http://en.wikipedia.org/wiki/Triangular_distribution
+
+        """
+        u = self.random()
+        try:
+            c = 0.5 if mode is None else (mode - low) / (high - low)
+        except ZeroDivisionError:
+            return low
+        if u > c:
+            u = 1.0 - u
+            c = 1.0 - c
+            low, high = high, low
+        return low + (high - low) * _sqrt(u * c)
+
+]]
+
+function math.random_triangular (low, high, mode)
+	local u, c = math.random(), 0.5
+
+	if mode then c = (mode - low) / (high - low) end
+
+	if c == math.huge then return low end
+
+	if u > c then
+		u = 1.0 - u
+		c = 1.0 - c
+		low, high = high, low
+	end
+
+	return low + (high - low) * math.sqrt(u * c)
+end
+
+--[[
+
+    def normalvariate(self, mu, sigma):
+        """Normal distribution.
+
+        mu is the mean, and sigma is the standard deviation.
+
+        """
+        # Uses Kinderman and Monahan method. Reference: Kinderman,
+        # A.J. and Monahan, J.F., "Computer generation of random
+        # variables using the ratio of uniform deviates", ACM Trans
+        # Math Software, 3, (1977), pp257-260.
+
+        random = self.random
+        while True:
+            u1 = random()
+            u2 = 1.0 - random()
+            z = NV_MAGICCONST * (u1 - 0.5) / u2
+            zz = z * z / 4.0
+            if zz <= -_log(u2):
+                break
+        return mu + z * sigma
+]]
+
+local NV_MAGICCONST = 4 * math.exp(-0.5) / math.sqrt(2.0)
+local LOG4 = math.log(4.0)
+local SG_MAGICCONST = 1.0 + math.log(4.5)
+local BPF = 53        		-- Number of bits in a float
+local RECIP_BPF = 2 ^ (-BPF)
+
+function math.random_normal (mu, sigma)
+
+	local random, z = math.random, nil
+
+    while true do
+		local u1 = random()
+		local u2 = 1.0 - random()
+		z = NV_MAGICCONST * (u1 - 0.5) / u2
+		local zz = z * z / 4.0
+		if zz <= -math.log(u2) then break end
+	end
+
+	return mu + z * sigma
+
+end
+
+--[[
+	def lognormvariate(self, mu, sigma):
+        """Log normal distribution.
+
+        If you take the natural logarithm of this distribution, you'll get a
+        normal distribution with mean mu and standard deviation sigma.
+        mu can have any value, and sigma must be greater than zero.
+
+        """
+        return _exp(self.normalvariate(mu, sigma))
+]]
+function math.random_lognormal(mu, sigma)
+
+	assert (sigma > 0)
+
+	return math.exp (math.normal (mu, sigma))
+end
+
+--[[
+	def expovariate(self, lambd):
+        """Exponential distribution.
+
+        lambd is 1.0 divided by the desired mean.  It should be
+        nonzero.  (The parameter would be called "lambda", but that is
+        a reserved word in Python.)  Returned values range from 0 to
+        positive infinity if lambd is positive, and from negative
+        infinity to 0 if lambd is negative.
+
+        """
+        # lambd: rate lambd = 1/mean
+        # ('lambda' is a Python reserved word)
+
+        # we use 1-random() instead of random() to preclude the
+        # possibility of taking the log of zero.
+        return -_log(1.0 - self.random()) / lambd
+]]
+function math.random_exponential (lambd, mean)
+	
+	if mean then 
+		-- in this case, `lambd` is the mean, so go transform it to an actual ratio.
+		lambd = 1 / lambd 
+	end
+
+	return -math.log(1.0 - math.random()) / lambd
+end
+
 return op
