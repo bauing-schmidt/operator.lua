20fea24276c46b709934ab7b472027352b5f6dcd
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-12-20 11:26:20 +0100

    Some cleanup

--- src/operator.lua
+++ src/operator.lua
@@ -1,70 +1,18 @@
 
 local op = {}
 
-function op.identity(...)
-	return ...	-- returns whatever I consume
-end
-
-function op.eternity(...)
-	return op.eternity(...)	-- infinite recursion
-end
-
-function op.noop(...)
-	-- simply do nothing
-end
-
-function op.forever(f, ...)
-	while true do f(...) end
-end
-
-function op.recv (...) 
-	return op.frecv(op.identity, op.noop, ...)
-end
-
-function op.frecv (f, g, s, ...)
-	if not g then g = function () end end
-	if s then return f(...) else return g(...) end
-end
-
-function op.add(a, b)
-	return a + b
-end
-
-function op.eq(a, b)
-	return a == b
-end
-
-function op.lt(a, b)
-	return a < b
-end
-
-function op.le(a, b)
-	return a <= b
-end
-
-function op.increment(a)
-	return op.add(a, 1)
-end
-
+function op.identity(...) return ... end
+function op.eternity(...) return op.eternity(...) end	-- via tail-recursion.
+function op.noop(...) end
+function op.forever(f, ...) while true do f(...) end end
+function op.precv (f, g) return function (s, ...) if s then return f(...) else return g(...) end end
+function op.add(a, b) return a + b end
+function op.eq(a, b) return a == b end
+function op.lt(a, b) return a < b end
+function op.le(a, b) return a <= b end
+function op.increment(a) return op.add(a, 1) end
 function op.apply (f, ...) return f(...) end
-
-function op.o (funcs)
-
-	return function (...) return table.foldr (funcs, op.apply, ...) end
-
-	--[[
-	return function (init)
-
-		for i = #funcs, 1, -1 do
-
-			local f = funcs[i]
-			init = table.pack (f (table.unpack (init)))
-		end
-		return table.unpack (init)
-	end
-	]]
-
-end
+function op.o (funcs) return function (...) return table.foldr (funcs, op.apply, ...) end end
 
 --------------------------------------------------------------------------------
 
